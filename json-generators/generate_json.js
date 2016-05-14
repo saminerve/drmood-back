7e9037d5c62e59e5a3cf7da1dbd97841d10f7802
Sami Maaref <maaref.sami@gmail.com>
2016-05-14 17:18:29 +0200

    add quotes

--- json-generators/generate_json.js
+++ json-generators/generate_json.js
@@ -18,8 +18,15 @@ var element;
 var ID = "ID"; var CONTENT_TYPE = "TYPE"; var ELEMENT_TYPE = "ELEMENT"; var EMOTIONS = "EMO"; var 	POINTS = "POINTS";
 var COMPL = "COMPL"; var LANGUAGE_DEFAULT = "DEFAULT"; var LANGUAGE_FR = "FR"; var LANGUAGE_EN = "EN"; var LANGUAGE_DE = "DE";
 
+var LANGUAGE_MAP = [ 
+{"key" : LANGUAGE_DEFAULT, "value" : "default"},
+{"key" : LANGUAGE_FR, "value" : "fr"},
+{"key" : LANGUAGE_EN, "value" : "en"},
+{"key" : LANGUAGE_DE, "value" : "de"}]
+
 var ELEMENT_TYPE_ENUM = {
 	Q : "Q",
+	N : "N",
 	R : "R"
 }
 
@@ -33,45 +40,73 @@ var CONTENT_TYPE_ENUM = {
 	TEXT : "TEXT",
 	//RESPONSES
 	PICTO : "PICTO",
-	IMAGE : "IMAGE"
+	IMAGE : "IMAGE",
+	QUOTE : "QUOTE"
 }
+
+function renderText(element) {
+    tradElement = {}
+	for( languageIndex in LANGUAGE_MAP ) {
+		languageElement = LANGUAGE_MAP[languageIndex]
+		tradElement[languageElement["value"]] = element[languageElement["key"]]
+	}
+
+	return tradElement	
+} 
+
+function renderQuote(element) {
+    tradElement = {"content" : {}, "author" : {}}
+	for( languageIndex in LANGUAGE_MAP ) {
+		languageElement = LANGUAGE_MAP[languageIndex]
+		if(element[languageElement["key"]] !== undefined){
+			var quotes = element[languageElement["key"]].trim().replace("[","").replace("]","");
+			quotes = quotes.split(";");
+
+			tradElement["content"][languageElement["value"]] = quotes[0]
+			tradElement["author"][languageElement["value"]] = quotes[1]
+		}
+	}
+
+	return tradElement	
+} 
 
 // Loop 
 for(var index in json_sheet){
 	element = json_sheet[index];
 
 	//Test if is question, id never used and question type is defined
-	if(	element[ELEMENT_TYPE] === ELEMENT_TYPE_ENUM.Q 
-			&& currentId !== element[ID] 
+	if(	element[ELEMENT_TYPE].trim() === ELEMENT_TYPE_ENUM.Q 
+			&& currentId !== element[ID].trim()
 			&& quizzMap[element[ID]] === undefined 
-			&& element[CONTENT_TYPE] !== undefined
-			&& (element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.TEXT //v 0.3 TEXT
-				|| element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.IMAGES //IMAGES
-				|| element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.TEXT_IMAGES //TEXT_IMAGES
-				|| element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.PHY //PHYSIOGNOMY
-				|| element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.IMAGE //ONE IMAGE
-				//|| element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.QUOTES // QUOTES
+			&& element[CONTENT_TYPE]!== undefined
+			&& (element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.TEXT //v 0.3 TEXT
+				|| element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.IMAGES //IMAGES
+				|| element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.TEXT_IMAGES //TEXT_IMAGES
+				|| element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.PHY //PHYSIOGNOMY
+				|| element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.IMAGE //ONE IMAGE
+				|| element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.QUOTES // QUOTES
 		)){
-		currentId = element[ID];
+
+		currentId = element[ID].trim();
 
 		quizzMap[currentId] = { "id" : currentId};
 		if(element[EMOTIONS] !== undefined)
-			quizzMap[currentId]["emotions"] = element[EMOTIONS].split("-");
+			quizzMap[currentId]["emotions"] = element[EMOTIONS].trim().split("-");
 
 		quizzMap[currentId]["question"] = {};
-		quizzMap[currentId]["question"]["type"] = element[CONTENT_TYPE];
-		if(element[CONTENT_TYPE] !== CONTENT_TYPE_ENUM.IMAGES)
-			quizzMap[currentId]["question"]["text"] = { "default" : element[LANGUAGE_DEFAULT], "fr" :  element[LANGUAGE_FR], "en" :  element[LANGUAGE_EN], "de" :  element[LANGUAGE_DE] };
+		quizzMap[currentId]["question"]["type"] = element[CONTENT_TYPE].trim();
+		if(element[CONTENT_TYPE].trim() !== CONTENT_TYPE_ENUM.IMAGES)
+			quizzMap[currentId]["question"]["text"] = renderText(element);
 		
 		if(element[COMPL] !== undefined){
-			if(element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.PHY){
-				var informations = element[COMPL].replace("[","").replace("]","").split(";");
+			if(element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.PHY){
+				var informations = element[COMPL].trim().replace("[","").replace("]","").split(";");
 				quizzMap[currentId]["question"]["area"] = {"zone" : informations[0], "position" : {"x" : informations[1], "y" : informations[2]}}
 				if(informations[3] !== undefined){
 					quizzMap[currentId]["question"]["area"]["icon"] = informations[3];
 				}
-			}else if(element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.IMAGE){
-				quizzMap[currentId]["question"]["image"] = element[COMPL];
+			}else if(element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.IMAGE){
+				quizzMap[currentId]["question"]["image"] = element[COMPL].trim();
 			}
 
 		}
@@ -80,27 +115,35 @@ for(var index in json_sheet){
 		quizzMap[currentId]["responses"] = [];
 		
 	//test if is answer, same id than last question, question is defined and is defined
-	} else if( element[ELEMENT_TYPE] === ELEMENT_TYPE_ENUM.R 
+	} else if( element[ELEMENT_TYPE] === ELEMENT_TYPE_ENUM.N 
+			&& currentId === element[ID] 
+			&& quizzMap[element[ID]] !== undefined ){
+		quizzMap[currentId]["none"] = renderText(element)
+
+
+	}else if( element[ELEMENT_TYPE] === ELEMENT_TYPE_ENUM.R 
 			&& currentId === element[ID] 
 			&& quizzMap[element[ID]] !== undefined
 			&& element[CONTENT_TYPE] !== undefined){
 
 		var response = {};
-		response["type"] = element[CONTENT_TYPE];
+		response["type"] = element[CONTENT_TYPE].trim();
 
-		if(element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.TEXT || element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.PICTO){
-			response["text"] = { "default" : element[LANGUAGE_DEFAULT], "fr" :  element[LANGUAGE_FR], "en" :  element[LANGUAGE_EN], "de" :  element[LANGUAGE_DE] };
-			if(element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.PICTO)
-				response["image"] = element[COMPL]
-		}else if(element[CONTENT_TYPE] === CONTENT_TYPE_ENUM.IMAGE){
-			response["image"] = element[LANGUAGE_DEFAULT];
+		if(element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.TEXT || element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.PICTO){
+			response["text"] = renderText(element);
+			if(element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.PICTO)
+				response["image"] = element[COMPL].trim()
+		} else if(element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.IMAGE){
+			response["image"] = element[LANGUAGE_DEFAULT].trim();
+		} else if(element[CONTENT_TYPE].trim() === CONTENT_TYPE_ENUM.QUOTE){
+			response["quote"] = renderQuote(element)
 		}
 
 		var quizzResponsePoints = [];
 
 		var points = element[POINTS];
 		if( points !== undefined ){
-			points = element[POINTS].replace("[","").replace("]","");
+			points = element[POINTS].trim().replace("[","").replace("]","");
 			var pointsArray = points.split(";");
 			
 			for(var i in pointsArray){
